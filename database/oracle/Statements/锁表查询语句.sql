05854a51fb40e1f271cbe8986922526122d19b83
itrunc <gomac@qq.com>
2015-08-20 16:52:28 +0800

    table lock statement

---
+++ database/oracle/Statements/锁表查询语句.sql
@@ -0,0 +1,16 @@
+SELECT /*+ rule */
+      s.username,
+       DECODE (l.TYPE,  'TM', 'TABLE LOCK',   'TX' , 'ROW LOCK',  NULL) LOCK_LEVEL,
+       o.owner,
+       o.object_name,
+       o.object_type,
+       s.sid,
+       s.serial#,
+       s.terminal,
+       s.machine,
+       s.program,
+       s.osuser
+  FROM v$session s , v$lock l , dba_objects o
+ WHERE l.sid = s.sid
+   AND l.id1 = o.object_id(+)
+   AND s.username IS NOT NULL;
